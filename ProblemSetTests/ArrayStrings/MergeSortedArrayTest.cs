f4484f88d298310b0e2db7dcf76627563ee7ffa9
brandon chin <chitangb.chin@gmail.com>
2024-08-18 12:16:08 -0400

    ArrayString: MergeSortedArrays

---
+++ ProblemSetTests/ArrayStrings/MergeSortedArrayTest.cs
@@ -0,0 +1,24 @@
+﻿using ProblemSets.ArrayStrings;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    [TestFixture]
+    public class MergeSortedArrayTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.Merge(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3), Is.EqualTo(new int[] { 1, 2, 2, 3, 5, 6 }));
+        }
+        [Test]
+        public void TestEmpty()
+        {
+            Assert.That(SolutionClass.Merge(new int[] { 0 }, 0, new int[] { 1 }, 1), Is.EqualTo(new int[] { 1 }));
+        }
+        [Test]
+        public void TestOneEl()
+        {
+            Assert.That(SolutionClass.Merge(new int[] { 1 }, 1, new int[] { }, 0), Is.EqualTo(new int[] { 1 }));
+        }
+    }
+}
---
+++ ProblemSets/ArrayStrings/MergeSortedArray.cs
@@ -0,0 +1,47 @@
+﻿/*
+Problem Statement: Merge Sorted Arrays
+
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should
+be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+*/
+
+namespace ProblemSets.ArrayStrings
+{
+    public class SolutionClass
+    {
+        /*
+         */
+        public static int[] Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            int p1 = m - 1;
+            int p2 = n - 1;
+
+            for (int p = m + n - 1; p >= 0; p--)
+            {
+                if (p2 < 0)
+                {
+                    break;
+                }
+
+                if (p1 >= 0 && nums1[p1] > nums2[p2])
+                {
+                    nums1[p] = nums1[p1];
+                    p1--;
+                }
+                else
+                {
+                    nums1[p] = nums2[p2];
+                    p2--;
+                }
+            }
+
+            return nums1;
+        }
+    }
+}
