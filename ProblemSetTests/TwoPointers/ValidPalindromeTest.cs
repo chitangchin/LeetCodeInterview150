adca6d26d77d5ad0936ca94fd8fb1be2090b22a3
brandon chin <chitangb.chin@gmail.com>
2024-08-18 17:14:47 -0400

    TwoPointers: Valid Palindrome

---
+++ ProblemSetTests/TwoPointers/ValidPalindromeTest.cs
@@ -0,0 +1,23 @@
+﻿using ProblemSets.TwoPointers.IsPalindrome;
+
+namespace ProblemSetTests.TwoPointers
+{
+    public class ValidPalindromeTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.IsPalindrome("A man, a plan, a canal: Panama"), Is.EqualTo(true));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.That(SolutionClass.IsPalindrome("race a car"), Is.EqualTo(false));
+        }
+        [Test]
+        public void TestEmptyInput()
+        {
+            Assert.That(SolutionClass.IsPalindrome(" "), Is.EqualTo(true));
+        }
+    }
+}
---
+++ ProblemSets/TwoPointers/ValidPalindrome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.TwoPointers.IsPalindrome
+{
+    public class SolutionClass
+    {
+        public static bool IsPalindrome(string s)
+        {
+            int leftPointer = 0;
+            int rightPointer = s.Length - 1;
+
+            while (leftPointer < rightPointer)
+            {
+                while (leftPointer < rightPointer && !Char.IsLetterOrDigit(s[leftPointer]))
+                {
+                    leftPointer++;
+                }
+                while (leftPointer < rightPointer && !Char.IsLetterOrDigit(s[rightPointer]))
+                {
+                    rightPointer--;
+                }
+                if (char.ToLower(s[rightPointer]) != char.ToLower(s[leftPointer]))
+                {
+                    return false;
+                }
+                leftPointer++;
+                rightPointer--;
+            }
+            return true;
+        }
+    }
+}
