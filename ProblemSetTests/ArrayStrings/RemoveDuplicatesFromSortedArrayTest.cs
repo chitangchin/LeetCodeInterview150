ebe3ee5bbce6ea46fe2d913afa23dec8850c410e
brandon chin <chitangb.chin@gmail.com>
2024-08-18 15:30:43 -0400

    ArrayStrings: RemoveDuplicatesFromSortedArray

---
+++ ProblemSetTests/ArrayStrings/RemoveDuplicatesFromSortedArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using ProblemSets.ArrayStrings.RemoveDuplicates;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    public class RemoveDuplicatesFromSortedArrayTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.RemoveDuplicates(new int[] { 1, 1, 2 }), Is.EqualTo(2));
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.That(SolutionClass.RemoveDuplicates(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }), Is.EqualTo(5));
+        }
+    }
+}
---
+++ ProblemSets/ArrayStrings/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.ArrayStrings.RemoveDuplicates
+{
+    public class SolutionClass
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            //Store index
+            int currIndex = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                //Check if element is unique
+                if (nums[i - 1] != nums[i])
+                {
+                    //If unique define the nums[currIndex] to nums[i]
+                    nums[currIndex++] = nums[i];
+                }
+            }
+            //Return the total count of unique elements
+            return currIndex;
+        }
+    }
+}
