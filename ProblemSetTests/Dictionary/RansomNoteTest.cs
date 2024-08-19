2e3279fe2959592262d80c1a7f9537e86a86ac13
chitangchin <chitangb.chin@gmail.com>
2024-08-19 16:04:12 -0400

    Dictionary: RansomNote

---
+++ ProblemSetTests/Dictionary/RansomNoteTest.cs
@@ -0,0 +1,23 @@
+﻿using ProblemSets.Dictionary.Ransom;
+
+namespace ProblemSetTests.Dictionary
+{
+    public class RansomNoteTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.CanConstruct("a", "b"), Is.False);
+        }
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.CanConstruct("aa", "ab"), Is.False);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.That(SolutionClass.CanConstruct("aa", "aab"), Is.True);
+        }
+    }
+}
---
+++ ProblemSets/Dictionary/RansomNote.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.Dictionary.Ransom
+{
+    public class SolutionClass
+    {
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<char, int> magazineDictionary = new Dictionary<char, int>();
+
+            for (int i = 0; i < magazine.Length; i++)
+            {
+                if (!magazineDictionary.ContainsKey(magazine[i]))
+                {
+                    magazineDictionary.Add(magazine[i], 1);
+                }
+                else
+                {
+                    magazineDictionary[magazine[i]]++;
+                }
+            }
+
+            for (int i = 0; i < ransomNote.Length; i++)
+            {
+                if (magazineDictionary.ContainsKey(ransomNote[i]) && magazineDictionary[ransomNote[i]] > 0)
+                {
+                    magazineDictionary[ransomNote[i]]--;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
