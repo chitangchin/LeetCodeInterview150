26dfe986c2507cdbb92c684e38687126ab6aff2d
brandon chin <chitangb.chin@gmail.com>
2024-08-18 21:27:07 -0400

    TwoPointers: IsSubSequence (Divide and Conquer)

---
+++ ProblemSetTests/TwoPointers/IsSubsequenceTest.cs
@@ -0,0 +1,18 @@
+﻿using ProblemSets.TwoPointers.IsSubsequence;
+
+namespace ProblemSetTests.TwoPointers
+{
+    public class IsSubsequenceTest
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(SolutionClass.IsSubsequence(new string ("abc"), new string("ahbgdc")), Is.True);
+        }
+        [Test]
+        public void Test2()
+        {
+            Assert.That(SolutionClass.IsSubsequence(new string("axc"), new string("ahbgdc")), Is.False);
+        }
+    }
+}
---
+++ ProblemSets/TwoPointers/IsSubsequence.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ProblemSets.TwoPointers.IsSubsequence
+{
+    public class SolutionClass
+    {
+        public static bool IsSubsequence(string s, string t)
+        {
+            //Divide and Conquere Greedy Approach
+
+            //Initiate left and right bound
+            int leftBound = s.Length;
+            int rightBound = t.Length;
+
+            //Create a recursive function
+            bool rec_isSubsequence(int leftIndex, int rightIndex)
+            {
+                //Base case
+                if (leftIndex == leftBound) return true;
+                if (rightIndex == rightBound) return false;
+
+                //Case 1:
+                if (s[leftIndex] == t[rightIndex])
+                {
+                    leftIndex++;
+                }
+                rightIndex++;
+
+                return rec_isSubsequence(leftIndex, rightIndex);
+            }
+
+            return rec_isSubsequence(0,0);
+        }
+    }
+}
