3d4e3c0e5472bf6c60fa5c8eb0fc73f79bbae54c
chitangchin <chitangb.chin@gmail.com>
2024-08-20 09:25:11 -0400

    Intervals: Summary Ranges

---
+++ ProblemSetTests/Intervals/SummaryRangesTest.cs
@@ -0,0 +1,22 @@
+﻿using ProblemSets.Intervals.SummaryRanges;
+
+namespace ProblemSetTests.Intervals
+{
+    [TestFixture]
+    public class SummaryRangesTest
+    {
+
+        [Test]
+        public void Test1()
+        {
+            var list = new List<string> { "0->2", "4->5", "7" };
+            Assert.That(SolutionClass.SummaryRanges(new int[] { 0, 1, 2, 4, 5, 7 }), Is.EqualTo(list));
+        }
+        [Test]
+        public void Test2()
+        {
+            var list = new List<string> { "0", "2->4", "6", "8->9" };
+            Assert.That(SolutionClass.SummaryRanges(new int[] { 0, 2, 3, 4, 6, 8, 9 }), Is.EqualTo(list));
+        }
+    }
+}
---
+++ ProblemSets/Intervals/SummaryRanges.cs
@@ -0,0 +1,43 @@
+﻿namespace ProblemSets.Intervals.SummaryRanges
+{
+    public class SolutionClass
+    {
+        public static IList<string> SummaryRanges(int[] nums)
+        {
+            //Given sorted unique integers \
+            /*
+            How to tell if range is increasing:
+            iterate through nums?
+            */
+            //Declare a string List result and initialize it to the new instance of a string List
+            IList<string> result = new List<string>();
+
+            //Edge case
+            if (nums.Length == 0) return result;
+            if (nums.Length == 1)
+            {
+                result.Add(nums[0].ToString());
+                return result;
+            }
+            int slowPointer = 0;
+            while (slowPointer < nums.Length)
+            {
+                int range = 1;
+                while (slowPointer + range < nums.Length && nums[slowPointer] + range == nums[slowPointer + range])
+                {
+                    range++;
+                }
+                string rangeResult = "";
+                int check = slowPointer;
+                rangeResult += $"{nums[slowPointer]}";
+                slowPointer += range;
+                if (check != slowPointer - 1)
+                {
+                    rangeResult += $"->{nums[slowPointer - 1]}";
+                }
+                result.Add(rangeResult);
+            }
+            return result;
+        }
+    }
+}
