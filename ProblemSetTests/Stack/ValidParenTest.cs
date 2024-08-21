274afb95b875d40598264684d5cfed2f99d52ece
brandon chin <chitangb.chin@gmail.com>
2024-08-20 22:16:48 -0400

    Stack: Valid Parentheses

---
+++ ProblemSetTests/Stack/ValidParenTest.cs
@@ -0,0 +1,13 @@
+﻿using ProblemSets.Stack.ValidParen;
+
+namespace ProblemSetTests.Stack
+{
+    public class ValidParenTest
+    {
+        [Test]
+        public void ValidParenTests() 
+        {
+            Assert.That(SolutionClass.IsValid(new string("()")), Is.True);
+        }
+    }
+}
---
+++ ProblemSets/Stack/ValidParen.cs
@@ -0,0 +1,37 @@
+﻿namespace ProblemSets.Stack.ValidParen
+{
+    public class SolutionClass
+    {
+        // Declared a private Dictionary variable called mappings (Encapsulation Pillar for OOP)
+        private Dictionary<char, char> mappings;
+
+        public SolutionClass()
+        {
+            mappings = new Dictionary<char, char> {
+            { ')', '(' }, { '}', '{' }, { ']', '[' }
+        };
+        }
+
+        public static bool IsValid(string s)
+        {
+            var solutionClass = new SolutionClass();
+            var stack = new Stack<char>();
+            foreach (var c in s)
+            {
+                if (solutionClass.mappings.ContainsKey(c))
+                {
+                    char topElement = stack.Count == 0 ? '#' : stack.Pop();
+                    if (topElement != solutionClass.mappings[c])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
