a99146bd280fe110aa6fea4c85abe8cfded96fba
chitangchin <chitangb.chin@gmail.com>
2024-08-21 09:51:45 -0400

    Linked List: Linked List Cycle

---
+++ ProblemSetTests/LinkedList/LinkedListCycleTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using ProblemSets.LinkedList.LinkedListCycle;
+
+namespace ProblemSetTests.LinkedList
+{
+    public class LinkedListCycleTest
+    {
+        [Test]
+        public void Test()
+        {    
+            ListNode linkedList = new ListNode(3);
+            linkedList.next = new ListNode(2);
+            linkedList.next = new ListNode(0);
+            linkedList.next = new ListNode(-4);
+
+            Assert.That(SolutionClass.HasCycle(linkedList), Is.False);
+        }
+    }
+}
---
+++ ProblemSets/LinkedList/LinkedListCycle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.LinkedList.LinkedListCycle
+{
+    public class ListNode(int x)
+    {
+        public int val = x;
+        public ListNode next;
+    }
+    public class SolutionClass
+    {
+        public static bool HasCycle(ListNode head)
+        {
+            if (head == null) return false;
+
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow != fast)
+            {
+                if (fast == null || fast.next == null) return false;
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+        }
+    }
+}
