27dd71c5d80f93851b174d0c39f3ac3cc0cac1d2
chitangchin <chitangb.chin@gmail.com>
2024-08-27 13:56:19 -0400

    Array/String: Best time to buy and sell stock

---
+++ ProblemSetTests/ArrayStrings/BestTimeToBuyAndSellStockTest.cs
@@ -0,0 +1,14 @@
+﻿using ProblemSets.ArrayStrings.BestTimeToBuyAndSellStock;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    public class BestTimeToBuyAndSellStockTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(SolutionClass.MaxProfit([7, 1, 5, 3, 6, 4]), Is.EqualTo(5));
+            Assert.That(SolutionClass.MaxProfit([7, 6, 4, 3, 1]), Is.EqualTo(0));
+        }
+    }
+}
---
+++ ProblemSets/ArrayStrings/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,32 @@
+﻿namespace ProblemSets.ArrayStrings.BestTimeToBuyAndSellStock
+{
+    public class SolutionClass
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            //Given price where prices[i] is price given stock on ith day
+            //[7,1,5,3,6,4]
+            // | |
+            //min, and current max profit
+
+            int currMin = prices[0];
+            int maxProfit = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < currMin)
+                {
+                    currMin = prices[i];
+                }
+                else
+                {
+                    if (prices[i] - currMin > maxProfit)
+                    {
+                        maxProfit = prices[i] - currMin;
+                    }
+                }
+            }
+            return maxProfit;
+        }
+    }
+}
