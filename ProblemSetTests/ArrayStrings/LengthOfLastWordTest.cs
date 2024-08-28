7482a9ef8eec2e7c2a4c6ab0421e030c71882ea7
chitangchin <chitangb.chin@gmail.com>
2024-08-28 15:36:11 -0400

    Array/String: LongestCommonPrefix and LengthOfLastWowrd

---
+++ ProblemSetTests/ArrayStrings/LengthOfLastWordTest.cs
@@ -0,0 +1,14 @@
+﻿using ProblemSets.ArrayStrings.LengthOfLastWord;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    public class LengthOfLastWordTest
+    {
+        public void Test()
+        {
+            Assert.That(SolutionClass.LengthOfLastWord("Hello World"), Is.EqualTo(5));
+            Assert.That(SolutionClass.LengthOfLastWord("   fly me   to   the moon  "), Is.EqualTo(4));
+            Assert.That(SolutionClass.LengthOfLastWord("luffy is still joyboy"), Is.EqualTo(6));
+        }
+    }
+}
