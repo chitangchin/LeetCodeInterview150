69fbe85862f20725a37d363db3b5b64450f0f3b9
chitangchin <chitangb.chin@gmail.com>
2024-08-29 08:35:25 -0400

    Binary General: InvertBInaryTree

---
+++ ProblemSets/BinaryTreeGeneral/InvertBinaryTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.BinaryTreeGeneral
+{
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+        public TreeNode? left = left;
+        public TreeNode? right = right;
+
+        public class InvertBinaryTreeLeet
+        {
+            public TreeNode InvertTree(TreeNode? root)
+            {
+                //Base case
+                if (root == null) return root;
+                //Invert
+                (root.right, root.left) = (InvertTree(root.left), InvertTree(root.right));
+                return root;
+            }
+        }
+    }
+}
