946135300561050224cff85193d1d98ce4cf33c8
chitangchin <chitangb.chin@gmail.com>
2024-08-30 08:38:40 -0400

    Array/String: Integer to Roman

---
+++ ProblemSetTests/ArrayStrings/IntegerToRomanTest.cs
@@ -0,0 +1,15 @@
+﻿using ProblemSets.ArrayStrings.IntegerToRoman;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    public class IntegerToRomanTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(IntegerToRomanLeet.IntegerToRoman(3749), Is.EqualTo("MMMDCCXLIX"));
+            Assert.That(IntegerToRomanLeet.IntegerToRoman(58), Is.EqualTo("LVIII"));
+            Assert.That(IntegerToRomanLeet.IntegerToRoman(1994), Is.EqualTo("MCMXCIV"));
+        }
+    }
+}
---
+++ ProblemSets/ArrayStrings/IntegerToRoman.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.ArrayStrings.IntegerToRoman
+{
+    public class IntegerToRomanLeet
+    {
+        private static readonly string[] thousands =
+{
+        "", "M", "MM", "MMM"
+    };
+
+        private static readonly string[] hundreds =
+        {
+        "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"
+    };
+
+        private static readonly string[] tens =
+        {
+        "",  "X",  "XX",  "XXX",  "XL", "L", "LX", "LXX", "LXXX", "XC"
+    };
+
+        private static readonly string[] ones =
+        {
+        "",  "I",  "II",  "III",  "IV", "V", "VI", "VII", "VIII", "IX"
+    };
+
+        public static string IntegerToRoman(int num)
+        {
+            return thousands[num / 1000] + hundreds[num % 1000 / 100] +
+                tens[num % 100 / 10] + ones[num % 10];
+        }
+    }
+}
