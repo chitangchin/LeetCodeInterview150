7756beeb5107119f43eac7557dcfc8e0e0f85baa
brandon chin <chitangb.chin@gmail.com>
2024-08-31 21:35:27 -0400

    Hashmap: Word Pattern

---
+++ ProblemSetTests/HashMap/WordPatternTest.cs
@@ -0,0 +1,16 @@
+﻿using ProblemSets.HashMap.WordPattern;
+
+namespace ProblemSetTests.HashMap
+{
+    public class WordPatternTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(WordPatternLeet.WordPattern("abba", "dog cat cat dog"), Is.True);
+            Assert.That(WordPatternLeet.WordPattern("abba", "dog cat cat fish"), Is.False);
+            Assert.That(WordPatternLeet.WordPattern("aaaa", "dog cat cat dog"), Is.False);
+
+        }
+    }
+}
---
+++ ProblemSets/HashMap/WordPattern.cs
@@ -0,0 +1,31 @@
+﻿namespace ProblemSets.HashMap.WordPattern
+{
+    public class WordPatternLeet
+    {
+        public static bool WordPattern(string pattern, string s)
+        {
+            Dictionary<char, string> hashMap = new Dictionary<char, string?>();
+            string[] resultArr = s.Split(" ");
+
+            if (resultArr.Length != pattern.Length)
+            {
+                return false;
+            }
+
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (hashMap.ContainsKey(pattern[i]))
+                {
+                    if (hashMap[pattern[i]] != resultArr[i]) return false;
+                }
+                else
+                {
+                    if (hashMap.ContainsValue(resultArr[i])) return false;
+                    hashMap[pattern[i]] = resultArr[i];
+                }
+            }
+            return true;
+        }
+    }
+}
