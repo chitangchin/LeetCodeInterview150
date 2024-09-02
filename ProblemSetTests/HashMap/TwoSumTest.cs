ef4df073a9a1960b95a6d73e28414f761899cb4e
brandon chin <chitangb.chin@gmail.com>
2024-09-02 19:24:25 -0400

    HashMap: TwoSum

---
+++ ProblemSetTests/HashMap/TwoSumTest.cs
@@ -0,0 +1,18 @@
+﻿using ProblemSets.HashMap.TwoSum;
+
+namespace ProblemSetTests.HashMap
+{
+    public class TwoSumTest
+    {
+        private readonly int[] expected = [0, 1];
+        private readonly int[] expected2 = [1, 2];
+        private readonly int[] expected3 = [0, 1];
+        [Test]
+        public void Test()
+        {
+            Assert.That(TwoSumLeet.TwoSum([2, 7, 11, 15], 9), Is.EqualTo(expected));
+            Assert.That(TwoSumLeet.TwoSum([3, 2, 4], 6), Is.EqualTo(expected2));
+            Assert.That(TwoSumLeet.TwoSum([3, 3], 6), Is.EqualTo(expected3));
+        }
+    }
+}
---
+++ ProblemSets/HashMap/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.HashMap.TwoSum
+{
+    public class TwoSumLeet
+    {
+        public static int[]? TwoSum(int[] nums, int target)
+        {
+            //One Pass Hashmap
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    return [map[nums[i]], i];
+                }
+                map[target - nums[i]] = i;
+            }
+            return null;
+        }
+    }
+}
