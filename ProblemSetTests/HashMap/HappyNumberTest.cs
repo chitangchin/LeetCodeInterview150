0dc4b78e5cc745241e1367b5329f1d7887459ad8
chitangchin <chitangb.chin@gmail.com>
2024-09-03 09:13:05 -0400

    Hashmap: Happy Number (SlowFastPointer Cycle)

---
+++ ProblemSetTests/HashMap/HappyNumberTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSets.HashMap.HappyNumber;
+
+namespace ProblemSetTests.HashMap
+{
+    public class HappyNumberTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(HappyNumberLeet.IsHappy(19), Is.True);
+            Assert.That(HappyNumberLeet.IsHappy(2), Is.False);
+        }
+    }
+}
---
+++ ProblemSets/HashMap/HappyNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.HashMap.HappyNumber
+{
+    public class HappyNumberLeet
+    {
+        public static int getNext(int n)
+        {
+            int totalSum = 0;
+            while (n > 0)
+            {
+                int digit = n % 10;
+                n = n / 10;
+                totalSum += digit * digit;
+            }
+            return totalSum;
+        }
+
+        public static bool IsHappy(int n)
+        {
+            //Slow and fast pointer to identify cycle
+            int slowPointer = n;
+            int fastPointer = getNext(n);
+            while (fastPointer != 1 && slowPointer != fastPointer)
+            {
+                slowPointer = getNext(slowPointer);
+                fastPointer = getNext(getNext(fastPointer));
+            }
+            return fastPointer == 1;
+        }
+    }
+}
