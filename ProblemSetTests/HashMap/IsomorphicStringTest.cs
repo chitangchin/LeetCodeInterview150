0786f49e13416b52a4b0df042a04115043543eb4
brandon chin <chitangb.chin@gmail.com>
2024-09-02 20:54:34 -0400

    Hashmap: IsIsomorphicString

---
+++ ProblemSetTests/HashMap/IsomorphicStringTest.cs
@@ -0,0 +1,15 @@
+﻿using ProblemSets.HashMap.IsomorphicString;
+
+namespace ProblemSetTests.HashMap
+{
+    public class IsomorphicStringTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(IsomorphicStringLeet.IsIsomorphic("egg", "add"), Is.True);
+            Assert.That(IsomorphicStringLeet.IsIsomorphic("foo", "bar"), Is.False);
+            Assert.That(IsomorphicStringLeet.IsIsomorphic("paper", "title"), Is.True);
+        }
+    }
+}
---
+++ ProblemSets/HashMap/IsomorphicString.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.HashMap.IsomorphicString
+{
+    public class IsomorphicStringLeet
+    {
+        public static bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, char> map = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (map.ContainsKey(s[i]))
+                {
+                    if (map[s[i]] != t[i]) return false;
+                }
+                else
+                {
+                    if (map.ContainsValue(t[i])) return false;
+                    map[s[i]] = t[i];
+                }
+            }
+            return true;
+        }
+    }
+}
