52c3ce20905bbf0156a83c05629338086ab7fd2f
chitangchin <chitangb.chin@gmail.com>
2024-09-03 08:47:10 -0400

    Hashmap: Valid Anagram

---
+++ ProblemSetTests/HashMap/ValidAnagramTest.cs
@@ -0,0 +1,14 @@
+﻿using ProblemSets.HashMap.ValidAnagram;
+
+namespace ProblemSetTests.HashMap
+{
+    public class ValidAnagramTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(ValidAnagramLeet.IsAnagram("anagram", "nagaram"), Is.True);
+            Assert.That(ValidAnagramLeet.IsAnagram("rat", "car"), Is.False);
+        }
+    }
+}
---
+++ ProblemSets/HashMap/ValidAnagram.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.HashMap.ValidAnagram
+{
+    public class ValidAnagramLeet
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (map.ContainsKey(s[i])) map[s[i]] += 1;
+                else
+                {
+                    map[s[i]] = 1;
+                }
+            }
+
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (map.ContainsKey(t[i]))
+                {
+                    map[t[i]] -= 1;
+                    if (map[t[i]] == 0) map.Remove(t[i]);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (map.Count != 0) return false;
+            return true;
+        }
+    }
+}
