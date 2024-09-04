13aa297b0b07ec1755a4b5bde194372256fd516b
chitangchin <chitangb.chin@gmail.com>
2024-09-04 16:54:15 -0400

    Array/String: Rotate Array

---
+++ ProblemSetTests/ArrayStrings/RotateArrayTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSets.ArrayStrings.RotateArray;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    public class RotateArrayTest
+    {
+        private readonly int[] expected = [5, 6, 7, 1, 2, 3, 4];
+        private readonly int[] expected2 = [3, 99, -1, -100];
+        [Test]
+        public void Test()
+        {
+            Assert.That(RotateLeet.Rotate([1, 2, 3, 4, 5, 6, 7], 3), Is.EqualTo(expected));
+            Assert.That(RotateLeet.Rotate([-1, -100, 3, 99], 2), Is.EqualTo(expected2));
+        }
+    }
+}
---
+++ ProblemSets/ArrayStrings/RotateArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.ArrayStrings.RotateArray
+{
+    public class RotateLeet
+    {
+        public static int[] Rotate(int[] nums, int k)
+        {
+            k = k % nums.Length;
+            int mid = nums.Length - k;
+            int[] left = nums[0..mid];
+            int[] right = nums[mid..];
+            var result = right.Concat(left).ToArray();
+            Array.Copy(result, nums, result.Length);
+            return nums;
+        }
+    }
+}
