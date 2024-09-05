b5e84531bd8a005832a6607efed54e30f5c12942
brandon chin <chitangb.chin@gmail.com>
2024-09-05 19:16:37 -0400

    Array/String: H Index

---
+++ ProblemSetTests/ArrayStrings/hIndexTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSets.ArrayStrings.HIndex;
+
+namespace ProblemSetTests.ArrayStrings
+{
+    public class HIndexTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(HIndexLeet.HIndex([3, 0, 6, 1, 5]), Is.EqualTo(3));
+            Assert.That(HIndexLeet.HIndex([1, 3, 1]), Is.EqualTo(1));
+            Assert.That(HIndexLeet.HIndex([0, 0, 4, 4]), Is.EqualTo(2));
+            Assert.That(HIndexLeet.HIndex([1, 2, 2, 3]), Is.EqualTo(2));
+        }
+    }
+}
