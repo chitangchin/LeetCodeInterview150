fb13fe7b20703f8212c4dc932e1deb3f83ff6d6e
chitangchin <chitangb.chin@gmail.com>
2024-09-09 14:24:38 -0400

    Array/String: Best TIme to Buy and Sell Stock II

---
+++ ProblemSets/ArrayStrings/BestTimeToBuyAndSellStockII.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.ArrayStrings.BestTimeToBuyAndSellStockII
+{
+    public class BestTimeToBuyAndSellStockIILeet
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int result = 0;
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] - prices[i - 1] > 0)
+                {
+                    result += prices[i] - prices[i - 1];
+                }
+            }
+            return result;
+        }
+    }
+}
