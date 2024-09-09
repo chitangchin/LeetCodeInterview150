8018b3478b84be4c6e8ed667b552c333e82122b6
chitangchin <chitangb.chin@gmail.com>
2024-09-09 14:54:47 -0400

    Array/String: Jump Game

---
+++ ProblemSets/ArrayStrings/JumpGame.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.ArrayStrings.JumpGame
+{
+    public class JumpGameLeet
+    {
+        public static bool CanJump(int[] nums)
+        {
+            int maxJump = nums[0];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i > maxJump) return false;
+
+                maxJump = Math.Max(maxJump, nums[i] + i);
+
+                if (maxJump >= nums.Length - 1) return true;
+            }
+            return false;
+        }
+    }
+}
