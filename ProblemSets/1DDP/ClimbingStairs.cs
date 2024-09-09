7c116bd5620057bffbe00de7fb0d2c4dc038ce87
chitangchin <chitangb.chin@gmail.com>
2024-09-09 08:27:43 -0400

    1ddp: Climbing stairs

---
+++ ProblemSets/1DDP/ClimbingStairs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets._1DDP.ClimbingStairs
+{
+    public class ClimbingStairsLeet
+    {
+        public static int ClimbStairs(int n)
+        {
+            //Fibonacci Sequence Pattern 1, 2, 3, 5, 8
+            if (n == 1) return 1;
+
+            int first = 1;
+            int second = 2;
+            for (int i = 3; i <= n; i++)
+            {
+                int third = first + second;
+                first = second;
+                second = third;
+            }
+            return second;
+        }
+    }
+}
