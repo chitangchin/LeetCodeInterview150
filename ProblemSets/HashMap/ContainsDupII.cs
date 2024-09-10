0467ab72ea37780a9efa76749dd5dc0d000750fc
brandon chin <chitangb.chin@gmail.com>
2024-09-10 19:15:35 -0400

    Hashmap: Contains Dup II

---
+++ ProblemSets/HashMap/ContainsDupII.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.HashMap.ContainsDupII
+{
+    public class ContainsDupIILeet
+    {
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (map.ContainsKey(nums[i]))
+                {
+                    if (Math.Abs(map[nums[i]] - i) <= k) return true;
+                    else
+                    {
+                        map[nums[i]] = i;
+                    }
+                }
+                else
+                {
+                    map[nums[i]] = i;
+                }
+            }
+            return false;
+        }
+    }
+}
