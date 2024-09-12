2aa24c25906ef4a76eb9aa7cb457d52c2f73c412
chitangchin <chitangb.chin@gmail.com>
2024-09-12 16:14:35 -0400

    Sliding Window: Length Of Longest Substring

---
+++ ProblemSetTests/SlidingWindow/LengthOfLongestSubstringTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSets.SlidingWindow.LengthOfLongestSubstring;
+
+namespace ProblemSetTests.SlidingWindow
+{
+    public class LengthOfLongestSubstringTest
+    {
+        [Test]
+        public void GivenTest()
+        {
+            Assert.That(LengthOfLongestSubstringLeet.LengthOfLongestSubstring("abcabcbb"), Is.EqualTo(3));
+            Assert.That(LengthOfLongestSubstringLeet.LengthOfLongestSubstring("bbbbb"), Is.EqualTo(1));
+            Assert.That(LengthOfLongestSubstringLeet.LengthOfLongestSubstring("pwwkew"), Is.EqualTo(3));
+        }
+        [Test]
+        public void CustomTest()
+        {
+            Assert.That(LengthOfLongestSubstringLeet.LengthOfLongestSubstring("abcdefghijklmnopqrstuvwxyza"), Is.EqualTo(26));
+            Assert.That(LengthOfLongestSubstringLeet.LengthOfLongestSubstring("bbbbbppppbbbbbaapppppppsssssllllllllaaaaaaaaaa"), Is.EqualTo(2));
+            Assert.That(LengthOfLongestSubstringLeet.LengthOfLongestSubstring("dksamdlamf asdladlaskd wqadsas"), Is.EqualTo(8));
+        }
+    }
+}
---
+++ ProblemSets/SlidingWindow/LengthOfLongestSubstring.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.SlidingWindow.LengthOfLongestSubstring
+{
+    public class LengthOfLongestSubstringLeet
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+            int left = 0;
+
+            for (int right = 1; right <= s.Length; right++)
+            {
+                // Move the left pointer to avoid duplicates
+                while (right <= s.Length && ContainsDuplicates(s[left..right]))
+                {
+                    left++;
+                }
+                // Update the result with the length of the current valid substring
+                result = Math.Max(result, right - left);
+            }
+            return result;
+        }
+
+        public static bool ContainsDuplicates(string substring)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            for (int i = 0; i < substring.Length; i++)
+            {
+                if (map.ContainsKey(substring[i]))
+                {
+                    // Duplicate found, return true
+                    return true;
+                }
+                else
+                {
+                    map[substring[i]] = 1;
+                }
+            }
+            // No duplicates found
+            return false;
+        }
+    }
+}
