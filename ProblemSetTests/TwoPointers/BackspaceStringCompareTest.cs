4596e5f85907a27b0c2ebfceec73f97556c470f4
chitangchin <chitangb.chin@gmail.com>
2024-09-18 09:20:54 -0400

    Two Pointer: Backspace String Compare

---
+++ ProblemSetTests/TwoPointers/BackspaceStringCompareTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSets.TwoPointers.BackspaceStringCompare;
+
+namespace ProblemSetTests.TwoPointers
+{
+    public class BackspaceStringCompareTest
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.That(BackspaceStringCompareLeet.BackspaceCompare("ab#c", "ad#c"), Is.True);
+            Assert.That(BackspaceStringCompareLeet.BackspaceCompare("ab##", "c#d#"), Is.True);
+            Assert.That(BackspaceStringCompareLeet.BackspaceCompare("a#c", "b"), Is.False);
+        }
+    }
+}
---
+++ ProblemSets/TwoPointers/BackspaceStringCompare.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.TwoPointers.BackspaceStringCompare
+{
+    public class BackspaceStringCompareLeet
+    {
+        public static bool BackspaceCompare(string S, string T)
+        {
+            int i = S.Length - 1, j = T.Length - 1;
+            int skipS = 0, skipT = 0;
+
+            while (i >= 0 || j >= 0)
+            {
+                while (i >= 0)
+                {
+                    if (S[i] == '#')
+                    {
+                        skipS++;
+                        i--;
+                    }
+                    else if (skipS > 0)
+                    {
+                        skipS--;
+                        i--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                while (j >= 0)
+                {
+                    if (T[j] == '#')
+                    {
+                        skipT++;
+                        j--;
+                    }
+                    else if (skipT > 0)
+                    {
+                        skipT--;
+                        j--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (i >= 0 && j >= 0 && S[i] != T[j])
+                {
+                    return false;
+                }
+
+                if ((i >= 0) != (j >= 0))
+                {
+                    return false;
+                }
+
+                i--;
+                j--;
+            }
+
+            return true;
+        }
+    }
+}
