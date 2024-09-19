217409217ee19e90a72e8d8a9feb42aa4ba95993
chitangchin <chitangb.chin@gmail.com>
2024-09-19 08:54:49 -0400

    Hashmap: Group Anagram

---
+++ ProblemSetTests/HashMap/GroupAnagramTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProblemSets.HashMap.GroupAnagram;
+
+namespace ProblemSetTests.HashMap
+{
+    public class GroupAnagramTest
+    {
+        private readonly static IList<IList<string>> expected = [["eat", "tea", "ate"], ["tan", "nat"], ["bat"]];
+        private readonly static IList<IList<string>> expected2 = [[""]];
+        private readonly static IList<IList<string>> expected3 = [["a"]];
+        [Test]
+        public void GivenTest()
+        {
+            Assert.That(GroupAnagramLeet.GroupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]), Is.EqualTo(expected));
+            Assert.That(GroupAnagramLeet.GroupAnagrams([""]), Is.EqualTo(expected2));
+            Assert.That(GroupAnagramLeet.GroupAnagrams(["a"]), Is.EqualTo(expected3));
+        }
+    }
+}
---
+++ ProblemSets/HashMap/GroupAnagram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.HashMap.GroupAnagram
+{
+    public class GroupAnagramLeet
+    {
+        public static IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            // Create a dictionary with the sorted string as the key
+            Dictionary<string, List<string>> hashMap = new Dictionary<string, List<string>>();
+
+            foreach (string str in strs)
+            {
+                // Sort the characters in the string to create the key
+                char[] chars = str.ToCharArray();
+                Array.Sort(chars);
+                string key = new string(chars);
+
+                // Add the original string to the correct group
+                if (hashMap.ContainsKey(key))
+                {
+                    hashMap[key].Add(str);
+                }
+                else
+                {
+                    hashMap[key] = new List<string> { str };
+                }
+            }
+
+            // Prepare the result
+            List<IList<string>> result = new List<IList<string>>();
+
+            foreach (var group in hashMap.Values)
+            {
+                result.Add(group);
+            }
+
+            return result;
+        }
+    }
+}
