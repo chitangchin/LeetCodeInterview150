6eca3974fc42163e5a401e45fa65e7dbaea95aee
chitangchin <chitangb.chin@gmail.com>
2024-09-24 15:57:37 -0400

    Binary Tree General: Path Sum

---
+++ ProblemSetTests/BinaryTreeGeneral/PathSumTest.cs
@@ -0,0 +1,29 @@
+﻿using ProblemSets.BinaryTreeGeneral.PathSum;
+
+namespace ProblemSetTests.BinaryTreeGeneral
+{
+    public class PathSumTest
+    {
+        [Test]
+        public void Test()
+        {
+            var root = new TreeNode(5,
+                            new TreeNode(4,
+                                new TreeNode(11,
+                                    new TreeNode(7),
+                                    new TreeNode(2)
+                                    ),
+                                null
+                                ),
+                            new TreeNode(8,
+                                new TreeNode(13),
+                                new TreeNode(4,
+                                    null,
+                                    new TreeNode(1)
+                                )
+                            )
+                        );
+            Assert.That(PathSumLeet.HasPathSum(root, 22), Is.True);
+        }
+    }
+}
---
+++ ProblemSets/BinaryTreeGeneral/PathSum.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSets.BinaryTreeGeneral.PathSum
+{
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+        public TreeNode left = left;
+        public TreeNode right = right;
+    }
+    public class PathSumLeet
+    {
+        public static bool HasPathSum(TreeNode root, int targetSum)
+        {
+            if (root == null) return false;
+
+            // Stack to hold pairs of (node, cumulative sum)
+            Stack<(TreeNode node, int currentSum)> nodeStack = new Stack<(TreeNode, int)>();
+            nodeStack.Push((root, root.val));
+
+            while (nodeStack.Count > 0)
+            {
+                var (node, currentSum) = nodeStack.Pop();
+
+                // If the node is null, skip it
+                if (node == null)
+                {
+                    continue;
+                }
+
+                // Check if it's a leaf node
+                if (node.left == null && node.right == null)
+                {
+                    // Check if the cumulative sum equals targetSum
+                    if (currentSum == targetSum)
+                    {
+                        return true;
+                    }
+                }
+
+                // Push the right child onto the stack with updated sum
+                if (node.right != null)
+                {
+                    nodeStack.Push((node.right, currentSum + node.right.val));
+                }
+
+                // Push the left child onto the stack with updated sum
+                if (node.left != null)
+                {
+                    nodeStack.Push((node.left, currentSum + node.left.val));
+                }
+            }
+            return false;
+        }
+    }
+}
